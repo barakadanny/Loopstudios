657c81b6c091aa07ab52d999b0921af0548fad8b
Danny <barakadan421@gmail.com>
2023-03-01 14:23:03 +0300

    Tailwind custom values added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkgray: '	#8c8c8c',
+        dimgray: '#696969',
+      },
+    },
+    screens: {
+      'xsm': {'max': '375px'},
+      'sm': {'max': '550px'},
+      'md': {'max': '767px'},
+      'lg': {'max': '991px'},
+      'xl': {'max': '1200px'}
+    },
   },
   plugins: [],
 }
