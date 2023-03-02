0e2f3c96d1cc5982a1a3fcc5e4f0ae91f381ead0
Danny <barakadan421@gmail.com>
2023-03-02 20:21:59 +0300

    Import features to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
+import Features from "./components/Features";
 
 function App() {
   return (
     <>
       <Nav/>
       <Hero />
+      <Features/>
     </>
   );
 }
